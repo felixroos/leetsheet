1f175aec422646635abf76d227805a20a796654e
Felix Roos <roos@entrecode.de>
2019-10-09 18:31:06 +0200

    added barrel file + rebuild

---
+++ lib/index.d.ts
@@ -0,0 +1,11 @@
+import { Snippet } from './Snippet';
+import { Sheet } from './Sheet';
+import { Measure } from './Measure';
+import { RealParser } from './RealParser';
+declare const _default: {
+    Snippet: typeof Snippet;
+    Sheet: typeof Sheet;
+    Measure: typeof Measure;
+    RealParser: typeof RealParser;
+};
+export default _default;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Snippet } from './Snippet';
+import { Sheet } from './Sheet';
+import { Measure } from './Measure';
+import { RealParser } from './RealParser';
+
+export default {
+  Snippet, Sheet, Measure, RealParser
+}
